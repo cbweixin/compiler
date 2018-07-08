8383c75672b4eb90dd2625a065a97bf02e44d7ad
xin wei <xin_wei@intuit.com>
2018-07-07 20:30:18 -0700

    first version of lexer, code dialog for debugging purpose.

--- bench/src/main/java/weixin/App.java
+++ bench/src/main/java/weixin/App.java
@@ -1,13 +1,20 @@
 package weixin;
 
+import weixin.exception.ParseException;
+import weixin.lex.Lexer;
+import weixin.lex.Token;
+import weixin.util.CodeDialog;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws ParseException {
+        System.out.println("Hello World!");
+        Lexer l = new Lexer(new CodeDialog());
+        for (Token t; (t = l.read()) != Token.EOF; ) {
+            System.out.println("=> " + t.getText());
+        }
     }
+
 }
