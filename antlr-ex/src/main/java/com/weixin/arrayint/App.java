e41fc019a7acbcec67de50f4d30b7d01f83a6e08
cbweixin <cbweixin@qq.com>
2020-02-14 14:08:12 -0800

    add a new ex

--- antlr-ex/src/main/java/com/weixin/arrayint/App.java
+++ antlr-ex/src/main/java/com/weixin/arrayint/App.java
@@ -1,5 +1,22 @@
 package com.weixin.arrayint;
 
+import com.weixin.arrayint.gen.ArrayInitLexer;
+import com.weixin.arrayint.gen.ArrayInitParser;
+import com.weixin.arrayint.gen.ArrayInitParser.ValueContext;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
 public class App {
+
+  public static void main(String[] args) {
+    ArrayInitLexer lexer = new ArrayInitLexer(CharStreams.fromString("{1,2,3}"));
+    ArrayInitParser parser = new ArrayInitParser(new CommonTokenStream(lexer));
+    ArrayInitParser.InitContext parsed = parser.init();
+
+    for(ValueContext v : parsed.value()){
+      System.out.printf("value : %s\n", v.getText());
+    }
+
+  }
 
 }
