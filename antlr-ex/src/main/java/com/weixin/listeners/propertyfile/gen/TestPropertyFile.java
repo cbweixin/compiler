7ddf02132fad284623a6a70cff79255153dffb05
cbweixin <cbweixin@qq.com>
2020-02-19 11:43:33 -0800

    the comments in properties file cause troubles : ``` line 1:0 token recognition error at: '#' line 1:1 token recognition error at: '-' line 1:2 token recognition error at: '-' line 1:3 token recognition error at: '-' line 2:0 token recognition error at: '#' line 2:1 token recognition error at: ' ' ```

---
+++ antlr-ex/src/main/java/com/weixin/listeners/propertyfile/gen/TestPropertyFile.java
@@ -0,0 +1,51 @@
+package com.weixin.listeners.propertyfile.gen;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import org.antlr.v4.misc.OrderedHashMap;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class TestPropertyFile {
+
+  public static class PropertyFileLoader extends PropertyFileBaseListener {
+
+    Map<String, String> props = new OrderedHashMap<String, String>();
+
+    @Override
+    public void exitProp(PropertyFileParser.PropContext ctx) {
+      String id = ctx.ID().getText(); // prop : ID '=' STRING '\n' ;
+      String value = ctx.STRING().getText();
+      System.out.printf("id : %s, value : %s\n", id, value);
+      props.put(id, value);
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    String inputFile = null;
+    if (args.length > 0) {
+      inputFile = args[0];
+    }
+    InputStream is = System.in;
+    if (inputFile != null) {
+      is = new FileInputStream(inputFile);
+    }
+
+    PropertyFileLexer lexer = new PropertyFileLexer(CharStreams.fromStream(is));
+    CommonTokenStream stream = new CommonTokenStream(lexer);
+    PropertyFileParser parser = new PropertyFileParser(stream);
+    ParseTree tree = parser.file();
+
+    // create a standard ANTLR parse tree walker
+    ParseTreeWalker walker = new ParseTreeWalker();
+    // create listener then feed to walker
+    PropertyFileLoader loader = new PropertyFileLoader();
+    walker.walk(loader, tree);        // walk parse tree
+    System.out.println(loader.props); // print results
+
+  }
+}
