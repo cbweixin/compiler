6581c3fac26ca07af3b3e65adb365e8db611c9d5
cbweixin <cbweixin@qq.com>
2020-02-25 12:40:20 -0800

    st tempate usage

--- antlr-ex/src/main/java/com/weixin/templatestest/TempateTest.java
+++ antlr-ex/src/main/java/com/weixin/templatestest/TempateTest.java
@@ -1,7 +1,10 @@
 package com.weixin.templatestest;
 
+import java.util.LinkedList;
+import java.util.List;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
 import org.stringtemplate.v4.STGroupString;
 
 public class TempateTest {
@@ -35,11 +38,60 @@ public class TempateTest {
 
   }
 
+  public static void test5() {
+    STGroup stg = new STGroupString("sqlTemplate(columns,condition) ::= <<select <columns> \n" +
+        "from table \n" +
+        "where 1=1 <if(condition)>and <condition><endif> >>");
+    ST sqlST = stg.getInstanceOf("sqlTemplate");
+    sqlST.add("columns", "order_id");
+    sqlST.add("condition", "dt='2017-04-04'");
+    System.out.println(sqlST.render());
+  }
+
+  public static void test6() {
+    STGroup stg = new STGroupString("sqlTemplate(columns,condition) ::= <<select <columns> " +
+        "from table " +
+        "where 1=1 <if(condition)>and <condition><endif> >>");
+    ST sqlST = stg.getInstanceOf("sqlTemplate");
+    sqlST.add("columns", "order_id");
+    sqlST.add("condition", "dt='2017-04-04'");
+    System.out.println(sqlST.render());
+  }
+
+  public static void test7() {
+    STGroup stg = new STGroupString("sqlTemplate(columns,condition) ::= <%select <columns> \n" +
+        "from table \n" +
+        "where 1=1 <if(condition)>and <condition><endif> %>");
+    ST sqlST = stg.getInstanceOf("sqlTemplate");
+    sqlST.add("columns", "order_id");
+    sqlST.add("condition", "dt='2017-04-04'");
+    System.out.println(sqlST.render());
+  }
+
+  public static void test8() {
+    STGroup stg = new STGroupFile(
+        "/Users/xinwei/Documents/weixin/study-antlr/antlr-ex/src/main/java/com/weixin/templatestest/dataExtarctSql.stg");
+    ST sqlST = stg.getInstanceOf("sqlTemplate");
+
+    List<String> columnList = new LinkedList<String>();
+    columnList.add("order_id");
+    columnList.add("price");
+    columnList.add("phone");
+    sqlST.add("columns", columnList);
+    sqlST.add("condition", "dt='2017-04-04'");
+    System.out.print(sqlST.render());
+
+  }
+
   public static void main(String[] args) {
     test1();
     test2();
     test3();
     test4();
+    test5();
+    test6();
+    test7();
+    test8();
   }
 
 }
