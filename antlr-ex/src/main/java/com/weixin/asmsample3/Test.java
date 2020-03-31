4c5a29b3adc63b9ad2fe6270e40408c31d478d04
cbweixin <cbweixin@qq.com>
2020-03-31 14:09:06 -0700

    add more examples

---
+++ antlr-ex/src/main/java/com/weixin/asmsample3/Test.java
@@ -0,0 +1,17 @@
+package com.weixin.asmsample3;
+
+public class Test {
+
+  private int num1 = 1;
+  public static int NUM1 = 100;
+  public int func(int a,int b){
+    return add(a,b);
+  }
+  public int add(int a,int b) {
+    return a+b+num1;
+  }
+  public int sub(int a, int b) {
+    return a-b-NUM1;
+  }
+
+}
--- antlr-ex/target/classes/com/weixin/listeners/propertyfile/PropertyFile.g4
+++
@@ -1,6 +0,0 @@
-grammar PropertyFile;
-file : prop+;
-prop : ID '=' STRING '\n';
-ID : [a-z]+;
-STRING : '"' .*? '"' ;
-LINE_COMMENT:   '#' .*? '\r'? '\n' -> skip ;
