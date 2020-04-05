679b9ef3b02d109234843a34063189183f33a616
cbweixin <cbweixin@qq.com>
2020-04-05 15:11:07 -0700

    add more tests:

--- antlr-ex/src/test/java/com/weixin/tpdsl/walking/patternmatch/SimplifyTest.java
+++ antlr-ex/src/test/java/com/weixin/tpdsl/walking/patternmatch/SimplifyTest.java
@@ -70,4 +70,28 @@ class SimplifyTest {
 
   }
 
+  @Test
+  public void testVecMul3() throws RecognitionException {
+    // Create lexer/parser to build trees from stdin
+    VecMathLexer lex = new VecMathLexer(
+        new ANTLRStringStream("x = 0 * [0,3*4,0*5]"));
+
+    CommonTokenStream tokens = new CommonTokenStream(lex);
+    VecMathParser p = new VecMathParser(tokens);
+    // launch parser by calling start rule
+    RuleReturnScope r = p.prog();
+    // get tree result
+    CommonTree t = (CommonTree) r.getTree();
+
+    System.out.println("Original tree: " + t.toStringTree());
+    assertEquals("(= x (* 0 (VEC 0 (* 3 4) (* 0 5))))", t.toStringTree(), "original tree value ");
+    // Traverse tree down then up, applying rewrite rules
+    CommonTreeNodeStream nodes = new CommonTreeNodeStream(t);
+    Simplify s = new Simplify(nodes);
+    // walk t, trace transforms
+    t = (CommonTree) s.downup(t, true);
+    System.out.println("Simplified tree: " + t.toStringTree());
+    assertEquals("(= x (VEC 0 (* 0 (* 3 4)) 0))", t.toStringTree());
+
+  }
 }
