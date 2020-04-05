02b2e8efb8e49e8a062ba809c60ec758ae917086
cbweixin <cbweixin@qq.com>
2020-04-05 14:48:36 -0700

    add more tests

--- antlr-ex/src/test/java/com/weixin/tpdsl/walking/patternmatch/SimplifyTest.java
+++ antlr-ex/src/test/java/com/weixin/tpdsl/walking/patternmatch/SimplifyTest.java
@@ -23,7 +23,7 @@ class SimplifyTest {
   }
 
   @Test
-  public void testVecMulBottomup() throws RecognitionException {
+  public void testVecMul1() throws RecognitionException {
     // Create lexer/parser to build trees from stdin
     VecMathLexer lex = new VecMathLexer(
         new ANTLRStringStream("x = 4 * [0,3*4,0*5]"));
@@ -43,6 +43,30 @@ class SimplifyTest {
     t = (CommonTree) s.downup(t, true);
     System.out.println("Simplified tree: " + t.toStringTree());
     assertEquals("(= x (VEC 0 (* 4 (* 3 4)) 0))", t.toStringTree());
+
+  }
+
+  @Test
+  public void testVecMul2() throws RecognitionException {
+    // Create lexer/parser to build trees from stdin
+    VecMathLexer lex = new VecMathLexer(
+        new ANTLRStringStream("x = [0,3*4,0*5] * 3"));
+    CommonTokenStream tokens = new CommonTokenStream(lex);
+    VecMathParser p = new VecMathParser(tokens);
+    // launch parser by calling start rule
+    RuleReturnScope r = p.prog();
+    // get tree result
+    CommonTree t = (CommonTree) r.getTree();
+
+    System.out.println("Original tree: " + t.toStringTree());
+    assertEquals("(= x (* (VEC 0 (* 3 4) (* 0 5)) 3))", t.toStringTree(), "original tree value ");
+    // Traverse tree down then up, applying rewrite rules
+    CommonTreeNodeStream nodes = new CommonTreeNodeStream(t);
+    Simplify s = new Simplify(nodes);
+    // walk t, trace transforms
+    t = (CommonTree) s.downup(t, true);
+    System.out.println("Simplified tree: " + t.toStringTree());
+    assertEquals("(= x (* (VEC 0 (* 3 4) 0) 3))", t.toStringTree());
 
   }
 
