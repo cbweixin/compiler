8b11a73bd85ba3755dcb461aab9d51f00c3406a7
cbweixin <cbweixin@qq.com>
2020-04-07 20:49:22 -0700

    add more tests

--- antlr-ex/src/test/java/com/weixin/tpdsl/symtab/monolithic/SymbolTableTest.java
+++ antlr-ex/src/test/java/com/weixin/tpdsl/symtab/monolithic/SymbolTableTest.java
@@ -32,4 +32,25 @@ class SymbolTableTest {
     assertEquals(expectedOutput, actuOutput);
     assertEquals("global:{i=<i:int>, float=float, int=int}", symtab.toString());
   }
+
+  @Test
+  public void testSymbolTable2() throws IOException, RecognitionException {
+    CymbolLexer lexer = new CymbolLexer(new ANTLRStringStream("float j;"));
+    CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+    CymbolParser parser = new CymbolParser(tokenStream);
+    // create symbol table
+    SymbolTable symtab = new SymbolTable();
+
+    PrintStream stdout = System.out;
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(baos));
+
+    String expectedOutput = "line 1: ref float\nline 1: def j\n";
+    parser.compilationUnit(symtab);
+    String actuOutput = baos.toString().replaceAll("\r","");
+    System.setOut(stdout);
+    System.out.println(actuOutput);
+    assertEquals(expectedOutput, actuOutput);
+    assertEquals("global:{j=<j:float>, float=float, int=int}", symtab.toString());
+  }
 }
