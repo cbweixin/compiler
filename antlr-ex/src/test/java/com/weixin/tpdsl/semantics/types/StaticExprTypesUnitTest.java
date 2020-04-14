f55af98e957ee829174d8c12edab284f8320edd4
cbweixin <cbweixin@qq.com>
2020-04-14 08:40:14 -0700

    add more tests

--- antlr-ex/src/test/java/com/weixin/tpdsl/semantics/types/StaticExprTypesUnitTest.java
+++ antlr-ex/src/test/java/com/weixin/tpdsl/semantics/types/StaticExprTypesUnitTest.java
@@ -44,9 +44,18 @@ class StaticExprTypesUnitTest {
     }
   };
 
+  static void showTypes(CymbolAST t, TokenRewriteStream tokens) {
+    if (t.evalType != null && t.getType() != CymbolParser.EXPR) {
+      System.out.printf("%-17s",
+          tokens.toString(t.getTokenStartIndex(),
+              t.getTokenStopIndex()));
+      String ts = t.evalType.toString();
+      System.out.printf(" type %-8s\n", ts);
+    }
+  }
 
   @Test
-  public void testStaticsExprTypes() throws RecognitionException, IOException {
+  public void testStaticsExprTypes_1() throws RecognitionException, IOException {
     CharStream input = new ANTLRInputStream(
         this.getClass().getClassLoader().getResourceAsStream("t.cymbol"));
     // CREATE PARSER AND BUILD AST
@@ -97,15 +106,59 @@ class StaticExprTypesUnitTest {
     //System.out.println("tree: "+t.toStringTree());
   }
 
-  static void showTypes(CymbolAST t, TokenRewriteStream tokens) {
-    if (t.evalType != null && t.getType() != CymbolParser.EXPR) {
-      System.out.printf("%-17s",
-          tokens.toString(t.getTokenStartIndex(),
-              t.getTokenStopIndex()));
-      String ts = t.evalType.toString();
-      System.out.printf(" type %-8s\n", ts);
-    }
+
+  @Test
+  public void testStaticsExprTypes_2() throws RecognitionException, IOException {
+    CharStream input = new ANTLRInputStream(
+        this.getClass().getClassLoader().getResourceAsStream("AB.cymbol"));
+    // CREATE PARSER AND BUILD AST
+    CymbolLexer lex = new CymbolLexer(input);
+    final TokenRewriteStream tokens = new TokenRewriteStream(lex);
+    CymbolParser p = new CymbolParser(tokens);
+    // create CymbolAST nodes
+    p.setTreeAdaptor(CymbolAdaptor);
+    // launch parser
+    RuleReturnScope r = p.compilationUnit();
+    // get tree result
+    CommonTree t = (CommonTree) r.getTree();
+
+    // CREATE TREE NODE STREAM FOR TREE PARSERS
+    CommonTreeNodeStream nodes = new CommonTreeNodeStream(t);
+    // where to find tokens
+    nodes.setTokenStream(tokens);
+    nodes.setTreeAdaptor(CymbolAdaptor);
+    SymbolTable symtab = new SymbolTable();
+    // DEFINE SYMBOLS
+    // pass symtab to walker
+    Def def = new Def(nodes, symtab);
+    // trigger define actions upon certain subtrees
+    def.downup(t);
+    // RESOLVE SYMBOLS, COMPUTE EXPRESSION TYPES
+    nodes.reset();
+    Types typeComp = new Types(nodes, symtab);
+    // trigger resolve/type computation actions
+    typeComp.downup(t);
+
+    // WALK TREE TO DUMP SUBTREE TYPES
+    TreeVisitor v = new TreeVisitor(new CommonTreeAdaptor());
+    TreeVisitorAction actions = new TreeVisitorAction() {
+      @Override
+      public Object pre(Object t) {
+        return t;
+      }
+
+      @Override
+      public Object post(Object t) {
+        showTypes((CymbolAST) t, tokens);
+        return t;
+      }
+    };
+    // walk in postorder, showing types
+    v.visit(t, actions);
+
+    //System.out.println("tree: "+t.toStringTree());
   }
+
 
 }
 
